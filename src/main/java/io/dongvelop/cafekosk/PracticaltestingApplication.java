d0e5d2e721072a3eafddb6d7dd202bc77bdcbac8
leedongyeop <ldy_1204@naver.com>
2023-12-27 22:57:37 +0900

    chore : project setting

---
+++ src/main/java/io/dongvelop/cafekosk/PracticaltestingApplication.java
@@ -0,0 +1,13 @@
+package io.dongvelop.cafekosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaltestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaltestingApplication.class, args);
+    }
+
+}
